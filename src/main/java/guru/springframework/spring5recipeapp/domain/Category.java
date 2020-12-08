8cab3ef839ef786949d0bed0c572ffd9c8d0d22c
weat0212 <weat0212@gmail.com>
2020-12-08 22:09:03 +0800

    Many to many JPA relationships. Bug Fix:UnitOfMeasure lose @Entity

--- src/main/java/guru/springframework/spring5recipeapp/domain/Category.java
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Category.java
@@ -1,23 +1,25 @@
 package guru.springframework.spring5recipeapp.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Set;
 
 /**
  * @author weat0212@gmail.com
  * @project spring5-recipe-app
  * @package guru.springframework.spring5recipeapp.domain
- * @date 2020/12/8 下午 09:14
+ * @date 2020/12/8 下午 09:32
  */
+
 @Entity
-public class UnitOfMeasure {
+public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String description;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
 
     public Long getId() {
         return id;
@@ -33,5 +35,13 @@ public class UnitOfMeasure {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
     }
 }
